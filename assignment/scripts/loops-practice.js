52cdbc55b97051fb94f9bc08e14991d11c9ebd16
Philip Ashton <philligan1@gmail.com>
2024-04-07 18:17:00 -0500

    through #2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -44,8 +44,8 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-for ('Some stars' of stars) {
-  console.log ('Some stars');
+for (let someStars of stars) {
+  console.log (someStars);
 }
 
 // 3. While loops  
